a8cfff8589841a569ebb7ae8972fa35d70bd085c
Alem Ayalew <alemayalew00@gmail.com>
2023-12-05 17:40:33 +0300

    todo.html

---
+++ bmi.js
@@ -0,0 +1,4 @@
+const mass = prompt("what is your mass?");
+const height = prompt("what is your height?");
+const bmi = mass/(height*height);
+console.log(bmi);
---
+++ practice.js
@@ -0,0 +1,23 @@
+let p = 1;
+let q = 1;
+ if(p===q){
+    var age = 25;
+    var age = 40;
+    let age2=60; //block scoped
+    const age3=75; //block scoped
+    let hello="welcome";
+    let sum =5+3;
+    let diff = 10-8;
+    let product = 2*3;
+    let quo = 15/3;
+    typeof(age2);
+    console.log(age);
+    console.log(age2); 
+    console.log(age3);
+    console.log(hello);
+    console.log(sum + diff);
+    console.log(quo + product);
+    //the 3rd = sign checks data type
+}
+
+
